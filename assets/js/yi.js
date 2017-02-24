a44ac15cd6f945aa3de7fc46665244da05b9e7c3
avataz <luasharapan@gmail.com>
2017-02-24 18:04:25 +0800

    Create yi.js

---
+++ assets/js/yi.js
@@ -0,0 +1,20 @@
+var password;
+
+var yi1="aliasak@gmail.com";
+var yi2="aliasak@gmail.com";
+var yi3="aliasak@gmail.com";
+
+password=prompt('Sila masukkan email anda yang betul',' ');
+
+if (password==yi1 || password==yi2 || password==yi3)
+  alert('Terima kasih! Sila klik untuk masuk.');
+else
+   {
+    window.location="password-ralat.html";
+    }
+	
+	
+<!-- http://www.pageresource.com/jscript/jpass.htm -->
+	
+	
+	
